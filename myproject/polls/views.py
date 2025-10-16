c47379ca2c8a346ac5a9f3912774fa6105820d86
Alicja Gaweł <ala.gawel.2007@gmail.com>
2025-10-16 08:53:43 +0200

    removed old view methods

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -6,21 +6,6 @@ from django.urls import reverse
 from django.views import generic
 from .models import Choice, Question
 
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
-
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
